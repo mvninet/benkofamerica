8853fa6e3dd1ca0f68f8f2786edffa8f585263c8
Mark Vinsloev Nielsen <mvni@netcompany.com>
2018-11-15 19:42:23 +0100

    removed test

--- GOTO/GOTO/Controllers/HomeController.cs
+++ GOTO/GOTO/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace GOTO.Controllers
         {
             ConnectorController conController = new ConnectorController();
             //conController.GetRoutes("http://wa-oadk.azurewebsites.net/api/routes/", "?weight=1&type=1&height=1&width=1&length=1");
-            conController.GetRoutes("http://nccesdkeit.azurewebsites.net/api/routes/", "?weight=1&typeOfGoods=1&month=1");
             return View();
         }
 
