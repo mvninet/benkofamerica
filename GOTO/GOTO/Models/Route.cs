65c1d976abf6e9b444a06df02ada85f58bfc3e54
Mark Vinsloev Nielsen <mvni@netcompany.com>
2018-11-15 13:51:01 +0100

    api Telstar setup

---
+++ GOTO/GOTO/Models/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GOTO.Models
+{
+    public class Route
+    {
+        public string FromCity { get; set; }
+        public string ToCity { get; set; }
+        public double Time { get; set; }
+        public double Price { get; set; }
+    }
+}
