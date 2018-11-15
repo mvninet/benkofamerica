d804992145be4cdb91808ad94f5410fe0fc5dc5d
maks <maks@netcompany.com>
2018-11-15 22:30:09 +0100

    Now possible to build again

---
+++ GOTO/GOTO/Frontend/js/SuccesPage.js
@@ -0,0 +1,9 @@
+﻿function showReceipt() {
+    var temp = document.getElementById("succesTemplate");
+    var clon = temp.content.cloneNode(true);
+    document.getElementById("confirmationPage").appendChild(clon);
+}
+
+function initSucces() {
+    showReceipt();
+}
