1d0f9e046e1c765cde1a8153a8ed14fe3e7aacb6
maks <maks@netcompany.com>
2018-11-15 13:48:06 +0100

    no message

--- GOTO/GOTO/Frontend/Searchpage.js
+++ GOTO/GOTO/Frontend/Searchpage.js
@@ -1,1 +1,7 @@
-﻿
+﻿function showInput_field() {
+    var temp = document.getElementsId("#fromSearch");
+    var item = temp.content.querySelector("#mainPage");
+    var a = document.importNode(item, true);
+    document.body.appendChild(a);
+
+}
