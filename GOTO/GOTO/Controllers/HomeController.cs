fd670c26ee7680892462b63b4e7f2b1e4a4a7149
JesperRavnNielsen <ravn.cs@gmail.com>
2018-11-16 02:59:12 +0100

    directed graph

--- GOTO/GOTO/Controllers/HomeController.cs
+++ GOTO/GOTO/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GOTO.Controllers;
+
 
 namespace GOTO.Controllers
 {
@@ -11,9 +13,22 @@ namespace GOTO.Controllers
     {
         public ActionResult Index()
         {
+            DatabaseWrapper db = new DatabaseWrapper(ConfigurationManager.AppSettings["DatabaseUserName"],
+                                                     ConfigurationManager.AppSettings["DatabasePassword"],
+                                                     ConfigurationManager.AppSettings["DatabaseConnectionURL"],
+                                                     ConfigurationManager.AppSettings["DatabaseTrustedConnection"].ToString(),
+                                                     ConfigurationManager.AppSettings["DatabaseName"].ToString(),
+                                                     Convert.ToInt32(ConfigurationManager.AppSettings["DatabaseConnectionTimeOut"]));
+
+            db.OpenConnection();
+            var testpricemultiplier = db.GetTypeCost("Recorded");
+            db.CloseConnection();
+            db.OpenConnection();
+            var test = db.GetOwnPricedSegments(testpricemultiplier);
+
             ShortestPathCalculator path = new ShortestPathCalculator();
-            path.SetUpEdgesAndCosts();
-            path.PrintShortestPath("A", "E");
+            path.SetUpEdgesAndCosts(test, true);
+            path.CalculateShortestPath("Congo", "Timbuktu");
             return View();
         }
 
