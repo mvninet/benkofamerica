11a93b980b4dbced61b35d8eedfc3d08a9843dd8
Mark Vinsloev Nielsen <mvni@netcompany.com>
2018-11-16 10:32:09 +0100

    #9 - fixed crash when unkown city

--- GOTO/GOTO/Controllers/HomeController.cs
+++ GOTO/GOTO/Controllers/HomeController.cs
@@ -19,37 +19,44 @@ namespace GOTO.Controllers
         public String getRoutes(string weight, string type, string height, string width, string depth, string from, string to)
         {
             List<List<PricedRouteSegment>> result = new List<List<PricedRouteSegment>>();
+            try
+            {
+                
 
-            DatabaseWrapper db = new DatabaseWrapper(ConfigurationManager.AppSettings["DatabaseUserName"],
-                                                     ConfigurationManager.AppSettings["DatabasePassword"],
-                                                     ConfigurationManager.AppSettings["DatabaseConnectionURL"],
-                                                     ConfigurationManager.AppSettings["DatabaseTrustedConnection"].ToString(),
-                                                     ConfigurationManager.AppSettings["DatabaseName"].ToString(),
-                                                     Convert.ToInt32(ConfigurationManager.AppSettings["DatabaseConnectionTimeOut"]));
+                DatabaseWrapper db = new DatabaseWrapper(ConfigurationManager.AppSettings["DatabaseUserName"],
+                                                         ConfigurationManager.AppSettings["DatabasePassword"],
+                                                         ConfigurationManager.AppSettings["DatabaseConnectionURL"],
+                                                         ConfigurationManager.AppSettings["DatabaseTrustedConnection"].ToString(),
+                                                         ConfigurationManager.AppSettings["DatabaseName"].ToString(),
+                                                         Convert.ToInt32(ConfigurationManager.AppSettings["DatabaseConnectionTimeOut"]));
 
-            db.OpenConnection();
-            var testpricemultiplier = db.GetTypeCost("Recorded");
-            db.CloseConnection();
-            db.OpenConnection();
-            var telstar = db.GetOwnPricedSegments(testpricemultiplier);
-            db.CloseConnection();
-            ConnectorController connector = new ConnectorController();
+                db.OpenConnection();
+                var testpricemultiplier = db.GetTypeCost("Recorded");
+                db.CloseConnection();
+                db.OpenConnection();
+                var telstar = db.GetOwnPricedSegments(testpricemultiplier);
+                db.CloseConnection();
+                ConnectorController connector = new ConnectorController();
 
-            var oceanic = connector.GetOceanicRoutes(Convert.ToDouble(weight), type, Convert.ToDouble(height), Convert.ToDouble(width), Convert.ToDouble(depth));
-            var eastindia = connector.GetEastIndiaRoutes(Convert.ToDouble(weight), type, "10-10-2018");
-            var map = telstar.Concat(oceanic).Concat(eastindia).ToList();
+                var oceanic = connector.GetOceanicRoutes(Convert.ToDouble(weight), type, Convert.ToDouble(height), Convert.ToDouble(width), Convert.ToDouble(depth));
+                var eastindia = connector.GetEastIndiaRoutes(Convert.ToDouble(weight), type, "10-10-2018");
+                var map = telstar.Concat(oceanic).Concat(eastindia).ToList();
 
-            ShortestPathCalculator fastPath = new ShortestPathCalculator();
-            fastPath.SetUpEdgesAndCosts(map, true);
-            var fastestPath = fastPath.CalculateShortestPath(from, to);
+                ShortestPathCalculator fastPath = new ShortestPathCalculator();
+                fastPath.SetUpEdgesAndCosts(map, true);
+                var fastestPath = fastPath.CalculateShortestPath(from, to);
 
-            ShortestPathCalculator cheapPath = new ShortestPathCalculator();
-            cheapPath.SetUpEdgesAndCosts(map, false);
-            var cheapestPath = cheapPath.CalculateShortestPath(from, to);
+                ShortestPathCalculator cheapPath = new ShortestPathCalculator();
+                cheapPath.SetUpEdgesAndCosts(map, false);
+                var cheapestPath = cheapPath.CalculateShortestPath(from, to);
 
-            result.Add(fastestPath);
-            result.Add(cheapestPath);
-            return JsonConvert.SerializeObject(result);
+                result.Add(fastestPath);
+                result.Add(cheapestPath);
+                return JsonConvert.SerializeObject(result);
+            } catch
+            {
+                return JsonConvert.SerializeObject(result);
+            }
         }
         }
 }
